0e09d87e744a1e44f1a5c50498fb6ddb5030d57b
lgcholoquinga <luisg.choloquinga@gamil.com>
2024-11-29 11:48:51 -0500

    create congi file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+
+  await app.listen(envs.port);
+  console.log(`Products Microservices running on port: ${envs.port}`);
 }
 bootstrap();
