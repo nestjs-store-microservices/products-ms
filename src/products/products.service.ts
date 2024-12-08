3545bac4721264ad981d2029ce19f2729dc22c22
lgcholoquinga <luisg.choloquinga@gamil.com>
2024-12-08 11:22:30 -0500

    crud products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,13 +1,9 @@
-import {
-  Injectable,
-  InternalServerErrorException,
-  Logger,
-  NotFoundException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { PrismaClient } from '@prisma/client';
+
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
 
 @Injectable()
@@ -31,9 +27,7 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
         data: response,
       };
     } catch {
-      throw new InternalServerErrorException(
-        'An error occurred while performing this process',
-      );
+      throw new RpcException('An error occurred while performing this process');
     }
   }
 
@@ -62,8 +56,12 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     const productDB = await this.product.findFirst({
       where: { id, available: true },
     });
+
     if (!productDB) {
-      throw new NotFoundException(`Product with id: ${id} not found.`);
+      throw new RpcException({
+        message: `Product with id: ${id} not found.`,
+        status: HttpStatus.BAD_REQUEST,
+      });
     }
 
     return productDB;
