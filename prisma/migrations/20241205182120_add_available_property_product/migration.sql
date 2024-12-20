fdcd3b1674149afc3e7281944d31818f81108221
lgcholoquinga <luisg.choloquinga@gamil.com>
2024-12-19 21:55:53 -0500

    prisma client

--- prisma/migrations/20241205182120_add_available_property_product/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "available" BOOLEAN NOT NULL DEFAULT true;
--- prisma/migrations/20241220025446_init/migration.sql
+++ prisma/migrations/20241220025446_init/migration.sql
@@ -4,8 +4,12 @@ CREATE TABLE "Product" (
     "name" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
     "description" TEXT,
+    "available" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE INDEX "Product_available_idx" ON "Product"("available");
